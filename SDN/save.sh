2d0dcc2079131098095f4e284a88e34f7c98e234
Jesraael Ramses González González <learsejsesmar@outlook.es>
2026-05-28 21:14:17 +0000

    Avance automatico: 28-05-2026 21:14:17

--- SDN/save.sh
+++ SDN/save.sh
@@ -3,7 +3,7 @@
 git add .
 
 # 2. Crear commit 
-msg="Avance automatico: $(date+'%d-%m-%Y %H:%M:%S')"
+msg="Avance automatico: $(date +'%d-%m-%Y %H:%M:%S')"
 git commit -m "$msg"
 
 # Empujar los cambios a Git Hub
